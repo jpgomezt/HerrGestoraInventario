4d4cb624514c2d0ed52e44b2e1350a13521282b7
Snatiolam <santiago.alzate1@hotmail.com>
2020-11-18 18:16:56 -0500

    Actualizacion con app.py

--- Sojas/alzate.py
+++ Sojas/alzate.py
@@ -6,6 +6,7 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import smtplib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
@@ -24,25 +25,37 @@ class User(UserMixin, db.Model):
     is_admin = db.Column(db.Boolean(), default = False, nullable = False )
     password = db.Column(db.String(80))
 
+
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(60))
     url_img = db.Column(db.String(255))
-    precio = db.Column(db.String(30))
+    precio = db.Column(db.Float)
     descrip = db.Column(db.String(255))
-    stock = db.Column(db.String(30))
-    descuento = db.Column(db.String(30))
-    inventario = db.Column(db.String(30))
+    stock = db.Column(db.Integer)
+    descuento = db.Column(db.Float)
+    inventario = db.Column(db.Integer)
+    tipo = db.Column(db.String(30))
+    genero = db.Column(db.String(10))
+    rojo = db.Column(db.Boolean(), default = False, nullable = False )
+    azul = db.Column(db.Boolean(), default = False, nullable = False )
+    verde = db.Column(db.Boolean(), default = False, nullable = False )
+    blanco = db.Column(db.Boolean(), default = False, nullable = False )
+    negro = db.Column(db.Boolean(), default = True, nullable = False )
+    Talla_S = db.Column(db.Boolean(), default = False, nullable = False )
+    Talla_M = db.Column(db.Boolean(), default = True, nullable = False )
+    Talla_L = db.Column(db.Boolean(), default = False, nullable = False )
+    Talla_XL = db.Column(db.Boolean(), default = False, nullable = False )
+    #colores = db.Column(db.Integer, db.ForeignKey('colores.id')) #ej: rojo,azul,verde
+    #tallas = db.Column(db.Integer, db.ForeignKey('tallas.id')) #ej: S,XL,M
 
-class ColoresDeProductos:
-    id = db.Column(db.Integer, primary_key=True)
-    id_producto = db.Column(db.String(60), db.ForeignKey('producto.id'), nullable = False)
-    id_color = db.Column(db.String(60), db.ForeignKey('color.id'), nullable = False)
 
-class Colors(db.Model):
+class Pedidos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60), unique=True)
-    hex = db.Column(db.String(60), unique=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('user.id'))
+    total_ropa_hombre = db.Column(db.Integer)
+    total_ropa_mujer = db.Column(db.Integer)
+    total_ropa_descuento = db.Column(db.Integer)
 
 
 
@@ -145,7 +158,21 @@ def consola_admin():
     else:
         return redirect(url_for('home'))
 
-
+@app.route('/consola_admin/informe')
+@login_required
+def informe():
+    if current_user.is_admin:
+        subject = "Informe"
+        text = "Hello World!"
+        message = 'Subject: {}\n\n{}'.format(subject, text)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("notificaciones.sojas@gmail.com", "Cl4v3d3s0j4s")
+        server.sendmail("notificaciones.sojas@gmail.com", "jpgomezt@eafit.edu.co", message)
+        server.quit()
+        return render_template("correo.html")
+    else:
+        return redirect(url_for('home'))
 
 
 @app.route('/consola_admin/cantidades', methods=['GET', 'POST'])
@@ -162,7 +189,70 @@ def cantidades():
             stock = request.form['stock']
             descuento = request.form['descuento']
             inventario = request.form['inventario']
-            prod = Producto(nombre = name, url_img = url_img, precio = precio, descrip = descrip, stock = stock, descuento = descuento, inventario = inventario)
+            color_rojo = False
+            color_azul = False
+            color_verde = False
+            color_negro = False
+            color_blanco = False
+            Talla_S = False
+            Talla_M = False
+            Talla_L = False
+            Talla_XL = False
+
+            #Para los colores de la ropa
+            try:
+                color = request.form['rojo']
+                color_rojo = True
+            except:
+                color_rojo = False
+            try:
+                color = request.form['azul']
+                color_azul = True
+            except:
+                color_azul = False
+            try:
+                color = request.form['verde']
+                color_verde = True
+            except:
+                color_verde = False
+            try:
+                color = request.form['negro']
+                color_negro = True
+            except:
+                color_negro = False
+            try:
+                color = request.form['blanco']
+                color_blanco = True
+            except:
+                color_blanco = False
+
+
+            #Para los tamaños de la ropa
+            try:
+                color = request.form['S']
+                Talla_S = True
+            except:
+                Talla_S = False
+            try:
+                color = request.form['M']
+                Talla_M = True
+            except:
+                Talla_M = False
+            try:
+                color = request.form['L']
+                Talla_L = True
+            except:
+                Talla_L = False
+            try:
+                color = request.form['XL']
+                Talla_XL = True
+            except:
+                Talla_XL = False
+
+
+            gender = request.form['genero']
+
+            prod = Producto(nombre = name, url_img = url_img, precio = precio, descrip = descrip, stock = stock, descuento = descuento, inventario = inventario, tipo = gender, rojo = color_rojo , azul = color_azul, verde = color_verde , negro = color_negro, blanco = color_blanco, Talla_S = Talla_S, Talla_M = Talla_M, Talla_L = Talla_L, Talla_XL = Talla_XL)
             try:
                 db.session.add(prod)
                 db.session.commit()
@@ -175,6 +265,7 @@ def cantidades():
         return redirect(url_for('home'))
 
 @app.route('/consola_admin/cantidades/delete/<int:id>')
+@login_required
 def delete_producto(id):
     if current_user.is_admin:
         product_to_delete = Producto.query.get_or_404(id)
@@ -189,10 +280,10 @@ def delete_producto(id):
 
 
 @app.route('/consola_admin/cantidades/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update_producto(id):
     if current_user.is_admin:
         productos = Producto.query.get_or_404(id)
-
         if request.method == 'POST':
                 productos.nombre = request.form['nombre']
                 productos.url_img = request.form['url_img']
@@ -201,6 +292,77 @@ def update_producto(id):
                 productos.stock = request.form['stock']
                 productos.descuento = request.form['descuento']
                 productos.inventario = request.form['inventario']
+                productos.tipo = request.form['genero']
+                color_rojo = False
+                color_azul = False
+                color_verde = False
+                color_negro = False
+                color_blanco = False
+                Talla_S = False
+                Talla_M = False
+                Talla_L = False
+                Talla_XL = False
+
+                #Para los colores de la ropa
+                try:
+                    color = request.form['rojo']
+                    color_rojo = True
+                except:
+                    color_rojo = False
+                try:
+                    color = request.form['azul']
+                    color_azul = True
+                except:
+                    color_azul = False
+                try:
+                    color = request.form['verde']
+                    color_verde = True
+                except:
+                    color_verde = False
+                try:
+                    color = request.form['negro']
+                    color_negro = True
+                except:
+                    color_negro = False
+                try:
+                    color = request.form['blanco']
+                    color_blanco = True
+                except:
+                    color_blanco = False
+
+                productos.rojo = color_rojo
+                productos.azul = color_azul
+                productos.verde = color_verde
+                productos.negro = color_negro
+                productos.blanco = color_blanco
+
+                #Para los tamaños de la ropa
+                try:
+                    color = request.form['S']
+                    Talla_S = True
+                except:
+                    Talla_S = False
+                try:
+                    color = request.form['M']
+                    Talla_M = True
+                except:
+                    Talla_M = False
+                try:
+                    color = request.form['L']
+                    Talla_L = True
+                except:
+                    Talla_L = False
+                try:
+                    color = request.form['XL']
+                    Talla_XL = True
+                except:
+                    Talla_XL = False
+                
+                productos.Talla_S = Talla_S
+                productos.Talla_M = Talla_M
+                productos.Talla_L = Talla_L
+                productos.Talla_XL = Talla_XL
+
                 try:
                     db.session.commit()
                     return redirect(url_for('cantidades'))
@@ -219,33 +381,18 @@ def product():
     productos = Producto.query.order_by(Producto.id).all()
     return render_template('products.html', productos=productos)
 
+@app.route('/products/producto/<int:id>', methods=['GET', 'POST'])
+def vista_producto(id):
+    return render_template('productos/vista_productos.html')
 
-def createColors():
-    colors = {
-            'rojo': '#CF2F27',
-            'azul':'#277ECF',
-            'verde': '#62CF27', 
-            'amarillo': '#C5CF27'
-            }
-    for color in colors:
-        new_color = Colors(nombre=color, hex=colors[color])
-        comp_nombre = Colors.query.filter_by(nombre=color).first()
-        comp_hex = Colors.query.filter_by(hex=colors[color]).first()
-        if comp_nombre is not None or comp_hex is not None:
-            pass
-        else:
-            db.session.add(new_color)
-            db.session.commit()
 
 
 if __name__ == '__main__':
-    createColors()
-
     #Creacion de la cuenta del Admin
     hashed_password = generate_password_hash("12345678", method='sha256')
-    new_user = User(username="admin", email="admin@admin.com",is_admin=True ,password=hashed_password)
+    new_user = User(username="admin", email="admon.sojas@gmail.com",is_admin=True ,password=hashed_password)
     comp_user = User.query.filter_by(username="admin").first()
-    comp_email = User.query.filter_by(email="admin@admin.com").first()
+    comp_email = User.query.filter_by(email="admon.sojas@gmail.com").first()
     if comp_user is not None or comp_email is not None:
         pass
     else:
